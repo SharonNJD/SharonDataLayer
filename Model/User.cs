2a76993cac3012cb78281187939944c20933fcc6
SharonNJD <SharonNJD@gmail.com>
2023-10-30 16:13:19 +0200

    Add project files.

---
+++ Model/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class User:BaseEntity
+    {
+        protected string firstName;
+        protected string lastName;
+        protected string email;
+        protected string password;
+        
+      
+        protected DateTime birthday;
+        protected bool gender;
+        protected string phoneNum;
+        protected bool isManager;
+        protected string RealId; 
+        
+        
+
+        public string FirstName { get { return this.firstName; } set { this.firstName = value; } }
+        public string LirstName { get { return this.lastName; } set { this.lastName = value; } }
+        public string Password { get { return this.password; } set { this.password = value; } }
+        public string realid { get { return this.RealId; } set { this.RealId = value; } }
+
+        public string Email { get { return this.email; } set { this.email = value; } }
+
+        public DateTime Birthday { get { return this.birthday; } set { this.birthday = value; } }
+        public bool Gender { get { return this.gender; } set { this.gender = value; } }
+        public bool ismanager { get { return this.isManager; } set { this.isManager = value; } }
+
+        public string Phonenum { get { return this.phoneNum; } set { this.phoneNum = value; } }
+
+        
+    }
+    public class UserList : List<User>
+    {
+        //בנאי ברירת מחדל - אוסף ריק
+        public UserList() { }
+        //המרה אוסף גנרי לרשימת משתמשים
+        public UserList(IEnumerable<User> list)
+            : base(list) { }
+        //המרה מטה מטיפוס בסיס לרשימת משתמשים
+        public UserList(IEnumerable<BaseEntity> list)
+            : base(list.Cast<User>().ToList()) { }
+    }
+
+}
---
+++ ViewModel/UserDB.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+
+
+namespace ViewModel
+{
+    public class UserDB : BaseDB
+    {
+        // מכין משתמש על פי בקשה ומחזיר אותה
+        protected override BaseEntity CreateModel(BaseEntity entity)
+        {
+            User user = entity as User;
+            user.Id = int.Parse(reader["id"].ToString());
+            user.FirstName = reader["firstName"].ToString();
+            user.LirstName = reader["lastName"].ToString();
+            user.Email = reader["email"].ToString();
+            user.Password = reader["password"].ToString();
+            user.Gender = bool.Parse(reader["gender"].ToString());
+            user.Phonenum = reader["phoneNum"].ToString();
+            user.ismanager = bool.Parse(reader["isManager"].ToString());
+            user.Birthday = DateTime.Parse(reader["birthday"].ToString());
+            user.realid = reader["RealId"].ToString();
+            return user;
+        }
+
+        protected override BaseEntity NewEntity()
+        {
+            return new User();
+        }
+        // מביא את כל המשתמשים הקיימים במערכת
+        public UserList SelectAll()
+        {
+            command.CommandText = "SELECT * FROM tblUsers";
+            UserList list = new UserList(ExecuteCommand());
+            return list;
+        }
+        // מביא את המשתמש שמבקשים על פי ID 
+        public User SelectById(int id)
+        {
+            command.CommandText = "SELECT * FROM tblUsers WHERE id=" + id;
+            UserList users = new UserList(ExecuteCommand());
+            if (users.Count == 0)
+                return null;
+            return users[0];
+        }
+        
+    }
+}
